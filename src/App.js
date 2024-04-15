d0d09f9bd8a0eb13f5058727fc840dfe6f208caa
janoc0504 <janoc0504@gmail.com>
2024-04-15 13:17:24 -0600

    first commit hw-38

--- src/App.js
+++ src/App.js
@@ -1,24 +1,46 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
 function App() {
+  // The state of todos list
+  const [todos, setTodos] = useState([]);
+
+  // Function for switching state of todos
+  const toggleTodoCompletion = (index) => {
+    const updatedTodos = [...todos];
+    updatedTodos[index].completed = !updatedTodos[index].completed;
+    setTodos(updatedTodos);
+  };
+
+  // Adding a new todo_
+  const handleFormSubmit = (event) => {
+    event.preventDefault();
+    const todoInput = event.target.elements.todoInput;
+    const todoText = todoInput.value.trim();
+    if (todoText) {
+      setTodos([...todos, { text: todoText, completed: false }]);
+      todoInput.value = '';
+    }
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <div className="App">
+        <div className="todo-list">
+          {todos.map((todo, index) => (
+              <div
+                  key={index}
+                  className={todo.completed ? "todo-item completed" : "todo-item"}
+                  onClick={() => toggleTodoCompletion(index)}
+              >
+                {todo.text}
+              </div>
+          ))}
+        </div>
+        <form onSubmit={handleFormSubmit}>
+          <input type="text" name="todoInput" placeholder="Enter todo" />
+          <button type="submit">Add Todo</button>
+        </form>
+      </div>
   );
 }
 
